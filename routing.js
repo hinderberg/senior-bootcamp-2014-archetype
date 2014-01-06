42945350715c43fc0a747047569f97d43435d30b
Eirik Stien <eirik.stien@bekk.no>
2014-01-06 11:12:07 +0100

    handling relative root path

--- routing.js
+++ routing.js
@@ -8,6 +8,10 @@ var messageResource = require('./message-resource');
 exports.setup = function(server) {
 
 
+
+	server.get('/', function(req, res) {
+		res.send("Hello world. JA!");
+	})
 	server.get('/message/:id', messageResource.get);
 
 }
