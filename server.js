198fede1f82f82ad5f1c1e5d5e5901a10e06beee
Eirik Stien <eirik.stien@bekk.no>
2014-01-06 13:59:50 +0100

    user repo

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 var routing = require('./routing');
 var appConfig = require('./lib/app-config')();
 
+
 routing.setup(app);
+
 
 app.listen(appConfig.port);
 console.log('Listening on port: ' + appConfig.port);
