90395af173703ee7686c1ac5d29a23e4254149b5
Eirik Stien <eirik.stien@bekk.no>
2014-01-07 15:43:02 +0100

    added user resource get

--- routing.js
+++ routing.js
@@ -1,6 +1,7 @@
 var messageResource = require('./lib/message/message-resource');
 var messageView = require('./lib/message/message-view');
 var graphsView = require('./lib/graphs/graphs-view');
+var userResource = require('./lib/user/user-resource');
 
 exports.setup = function(server) {
 
@@ -8,6 +9,7 @@ exports.setup = function(server) {
 	server.get('/graphs', graphsView.graphs);
 	server.get('/message/:id', messageResource.get);
 	server.get('/messages', messageResource.list);
-  server.post('/push', messageResource.insert);
+    server.post('/push', messageResource.insert);
+    server.get('/employee/:id', userResource.get);
 
 };
