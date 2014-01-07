7ed000511dfc43dcf3f910c909b4991865de12b3
Hans Magnus Inderberg <hinderberg@gmail.com>
2014-01-07 14:21:16 +0100

    Gui stuff

--- routing.js
+++ routing.js
@@ -1,10 +1,13 @@
 var messageResource = require('./lib/message/message-resource');
 var messageView = require('./lib/message/message-view');
+var graphsView = require('./lib/graphs/graphs-view');
 
 exports.setup = function(server) {
 
 	server.get('/', messageView.messages);
+	server.get('/graphs', graphsView.graphs);
 	server.get('/message/:id', messageResource.get);
 	server.get('/messages', messageResource.list);
-    server.post('/push', messageResource.insert);
+  server.post('/push', messageResource.insert);
+
 };
