92bd42287b474484f98d6a3b981b0b8c5bfe1234
Eirik Stien <eirik.stien@bekk.no>
2014-01-07 14:12:18 +0100

    added car test file

---
+++ test/car-repository-tests.js
@@ -0,0 +1,11 @@
+var chai = require("chai");
+var expect = chai.expect;
+
+var carRepository = require('./../lib/car/car-repository');
+
+describe('something', function(){
+    
+    
+    expect(1).to.equal(2);
+
+});
